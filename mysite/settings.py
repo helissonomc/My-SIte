b7a94acf3777f1c739062b0551b362c5a64d242f
- Helisson M <helissonom@dcomp.ufs.br>
2021-11-30 10:41:42 -0300

    'aaaaaaaaaaaaaa'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)q35n!mr&2qe%edurxd@1pw$0+qrkkrk-lw3202i^99&rsu-ik
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://helisson-site.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['helisson-site.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
